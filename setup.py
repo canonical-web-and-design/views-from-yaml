49278e91550ebf9e74680f8c1331d34113a908c7
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-01 10:50:59 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='canonicalwebteam.views-from-yaml',
-    version='0.1.1',
+    version='0.1.2',
     author='Canonical webteam',
     author_email='robin+pypi@canonical.com',
     url='https://github.com/canonical-webteam/views-from-yaml',
