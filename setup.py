16b4dc3198c2548c9c6b361c78ed0b768b150ca5
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-01 13:50:29 +0100

    Sort out dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='canonicalwebteam.views-from-yaml',
-    version='0.2.0',
+    version='0.2.1',
     author='Canonical webteam',
     author_email='robin+pypi@canonical.com',
     url='https://github.com/canonical-webteam/views-from-yaml',
@@ -16,7 +16,8 @@ setup(
     ),
     long_description=open('README.rst').read(),
     install_requires=[
-        'Django==1.3'
+        'Django >= 1.3',
+        'PyYaml >= 3.0'
     ],
 )
 
